3a8692a1085a48dccd78e7c6b919c86dcb8fac62
Caio de Moura Navas <caio@caiowebdev.com.br>
2022-08-27 11:13:47 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="ISO-8859-1">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo get_bloginfo( 'title' ); ?> - <?php echo get_bloginfo( 'description' )?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <main>
+        <div class="container-fluid">
+            <div class="row bg-primary px-3 py-1 text-white">            
+                <span class="hashtag">#OMAIORCAMPEÃODOBRASIL</span>
+            </div>
+        </div>
---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Homepage
+ * Description: Homepage do template SEP RD /blog.
+ *
+ */
+
+//Incorporando a página header.php
+//(conteúdos do cabeçalho da homepage)
+get_header();
+?>
+
+<?php
+//Incorporando a página footer.php
+//(conteúdos do rodpé da homepage)
+get_footer();
